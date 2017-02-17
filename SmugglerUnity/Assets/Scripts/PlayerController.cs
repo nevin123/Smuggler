10371f419be512890fa17de3505be9b6baaa735d
nevin123 <nevin-wouters@hotmail.com>
2017-02-17 14:14:56 +0100

    Networking v1.0

---
+++ SmugglerUnity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent (typeof(PlayerMotor))]
+public class PlayerController : NetworkBehaviour {
+
+    PlayerMotor Motor;
+
+    bool jump;
+    float jumpTimer;
+
+    void Start()
+    {
+        //Remore the script if its not the local player
+        if(!isLocalPlayer)
+        {
+            Destroy(this);
+            return;
+        }
+
+        Motor = GetComponent<PlayerMotor>();
+
+        jumpTimer = Motor.DefaultJumpTime + Motor.JumpBoosterDelay;
+    }
+
+	void FixedUpdate()
+    {
+        //Move Player
+        Motor.MovePlayer(Input.GetAxis("Horizontal"));
+
+        //Jump
+        if (Input.GetButton("Jump"))
+        {
+            //Down jump button
+            if (Motor.isGrounded && !jump)
+            {
+                jump = true;
+                Motor.Jump(0, true);
+            }
+        } else 
+        {
+            //Release jump button
+            jump = false;
+            jumpTimer = Motor.DefaultJumpTime + Motor.JumpBoosterDelay;
+        }
+        
+        //Jump
+        if (jump)
+        {
+            float _deltaTime = jumpTimer / Motor.DefaultJumpTime;
+
+            jumpTimer -= Time.fixedDeltaTime;
+
+            //Wait for delaytime
+            if(jumpTimer < Motor.DefaultJumpTime)
+                Motor.Jump(_deltaTime, false);
+
+            //Stop when time is over
+            if (jumpTimer <= 0)
+            {
+                jump = false;
+                jumpTimer = Motor.DefaultJumpTime + Motor.JumpBoosterDelay;
+            }
+        }
+    }
+}
---
+++ SmugglerUnity/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody2D))]
+public class PlayerMotor : MonoBehaviour {
+
+    [Header("Movement Values")]
+    public float MovementSpeed = 50;
+    public float JumpStrength = 10;
+    public float DefaultJumpTime = 1;
+    public float JumpBoosterDelay = 0.2f;
+
+    Rigidbody2D rb;
+
+    [HideInInspector]
+    public bool isGrounded = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    /// <summary>
+    /// Move Player
+    /// </summary>
+    /// <param name="_direction">The direction the player has to move towards</param>
+    public void MovePlayer(float _direction)
+    {
+        float _movementValue = _direction * MovementSpeed * Time.deltaTime;
+        rb.velocity = new Vector2(_movementValue, rb.velocity.y);
+    }
+
+    /// <summary>
+    /// Make the player Jump
+    /// </summary>
+    public void Jump(float _deltaTime, bool _start)
+    {
+        if (_start)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, JumpStrength);
+        } else
+        {
+            rb.velocity = rb.velocity + new Vector2(0, JumpStrength * 2 * _deltaTime * Time.fixedDeltaTime);
+        }
+        
+        Debug.Log(rb.velocity.y);
+    }
+
+    /// <summary>
+    /// On Collision Enter
+    /// </summary>
+    /// <param name="other"></param>
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        //Reset is grounded
+        if (other.gameObject.layer == LayerMask.NameToLayer("Environment"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    /// <summary>
+    /// On Collision Exit
+    /// </summary>
+    /// <param name="other"></param>
+    void OnCollisionExit2D(Collision2D other)
+    {
+        //Reset is grounded
+        if (other.gameObject.layer == LayerMask.NameToLayer("Environment"))
+        {
+            isGrounded = false;
+        }
+    }
+}
