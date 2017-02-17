d29777831e066bbc2727bf36a49b1d48489c387b
Mila <milabits@gmail.com>
2017-02-17 18:45:09 +0100

    Added basic camera
    Added basic camera

---
+++ SmugglerUnity/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Camera playerCamera;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        playerCamera.transform.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 2, -8);
+    }
+}
