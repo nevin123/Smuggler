a2e36f697218c7acbea676f981605877ff4b76a0
Mila <milabits@gmail.com>
2017-02-23 22:11:06 +0100

    Added spotlight

---
+++ SmugglerUnity/Assets/Scripts/SpotlightController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class SpotlightController : MonoBehaviour {
+
+    GameObject spotlight;
+
+    float startRotationX;
+    public float rotationSpeed;
+    public float rotationLimit;
+
+    private float rotationMax;
+    private float rotationMin;
+    private bool goBack;
+
+    // Use this for initialization
+    void Start() {
+        spotlight = transform.GetChild(0).gameObject;
+
+        startRotationX = spotlight.transform.localEulerAngles.x;
+
+        rotationMax = startRotationX + rotationLimit;
+        rotationMin = startRotationX - rotationLimit;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (spotlight.transform.localEulerAngles.x < rotationMax) {
+            goBack = true;
+        }
+        if (spotlight.transform.localEulerAngles.x > rotationMin) {
+            goBack = false;
+        }
+
+        Debug.Log(string.Format("{0} : {1}({2}) : {2}", rotationMin, spotlight.transform.localEulerAngles.x, goBack, rotationMax));
+
+        if (!goBack) {
+            spotlight.transform.Rotate(Vector3.up, rotationSpeed);
+        }
+        else {
+            spotlight.transform.Rotate(Vector3.up, -rotationSpeed);
+        }
+    }
+}
