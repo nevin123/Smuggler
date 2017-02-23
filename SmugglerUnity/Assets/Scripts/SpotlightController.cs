c58940e79eeb9ec3a60bb3812066ccdd96558f0a
Mila <milabits@gmail.com>
2017-02-23 21:47:54 +0100

    Fixaroo 3

---
+++ SmugglerUnity/Assets/Scripts/SpotlightController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class SpotlightController : MonoBehaviour {
+
+    float startRotationX;
+    public float rotationSpeed;
+    public float rotationLimit;
+
+    private float rotationMax;
+    private float rotationMin;
+    private bool goBack;
+
+    // Use this for initialization
+    void Start() {
+        startRotationX = transform.localEulerAngles.x;
+
+        rotationMax = startRotationX + rotationLimit;
+        rotationMin = startRotationX - rotationLimit;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (transform.localEulerAngles.x < rotationMax) {
+            goBack = true;
+        }
+        if (transform.localEulerAngles.x > rotationMin) {
+            goBack = false;
+        }
+
+        Debug.Log(string.Format("{0} : {1}({2}) : {2}", rotationMin, transform.localEulerAngles.x, goBack, rotationMax));
+
+        if (!goBack) {
+            transform.Rotate(Vector3.up, rotationSpeed);
+        }
+        else {
+            transform.Rotate(Vector3.up, -rotationSpeed);
+        }
+    }
+}
