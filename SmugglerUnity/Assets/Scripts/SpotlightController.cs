2479bb1fd755f86ebef20fe7c57d2b9802e62169
Mila <milabits@gmail.com>
2017-02-24 23:15:33 +0100

    Small refactor/code cleanup

--- SmugglerUnity/Assets/Scripts/SpotlightController.cs
+++ SmugglerUnity/Assets/Scripts/SpotlightController.cs
@@ -7,6 +7,9 @@ public class SpotlightController : MonoBehaviour {
     public float rotationSpeed;
     private float speed;
 
+    public float maxLimit;
+    public float minLimit;
+
     private bool hitMax = false;
 
     // Use this for initialization
@@ -16,56 +19,26 @@ public class SpotlightController : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        Debug.Log(string.Format("rotationMax: {0} rotationMin: {1} hitMax: {3}\neulerAngles: {2}", rotationLimit, 360 - rotationLimit, transform.rotation.eulerAngles.z, hitMax));
+        CameraLoop();
+    }
 
+    private void CameraLoop() {
+        //Debug.Log(string.Format("rotationMax: {0} rotationMin: {1} hitMax: {3}\neulerAngles: {2}", rotationLimit, 360 - rotationLimit, transform.rotation.eulerAngles.z, hitMax));
         if (!hitMax) {
-            if (transform.rotation.eulerAngles.z >= rotationLimit - 2 &&
-                transform.rotation.eulerAngles.z <= rotationLimit + 2) {
+            if (transform.rotation.eulerAngles.z >= maxLimit - 2 &&
+                transform.rotation.eulerAngles.z <= maxLimit + 2) {
                 hitMax = true;
             }
             speed = rotationSpeed;
         }
 
         if (hitMax) {
-            if (transform.rotation.eulerAngles.z >= 360 - rotationLimit - 2 &&
-                transform.rotation.eulerAngles.z <= 360 - rotationLimit + 2) {
+            if (transform.rotation.eulerAngles.z >= 360 - minLimit - 2 &&
+                transform.rotation.eulerAngles.z <= 360 - minLimit + 2) {
                 hitMax = false;
             }
             speed = -rotationSpeed;
         }
         transform.Rotate(Vector3.forward, speed);
-
-        //if (!hitMax)
-        //{
-        //    if (rotationLimit > transform.rotation.eulerAngles.z)
-        //    {
-        //        transform.Rotate(Vector3.forward, rotationSpeed);
-        //    }
-        //    else
-        //    {
-        //        hitMax = true;
-        //    }
-        //}
-        //if (hitMax)
-        //{
-        //    if (transform.rotation.eulerAngles.z >= 0 && transform.rotation.eulerAngles.z <= rotationLimit ||
-        //        transform.rotation.eulerAngles.z <= 360 && transform.rotation.eulerAngles.z > rotationLimit)
-        //    {
-
-        //        transform.Rotate(Vector3.back, rotationSpeed);
-
-        //    }
-        //    else if (transform.rotation.eulerAngles.z <= 360 - rotationLimit &&
-        //             transform.rotation.eulerAngles.z > rotationLimit + 1)
-        //    {
-        //        hitMax = false;
-        //    }
-
-        //    //if (rotationMin < transform.rotation.eulerAngles.z || rotationMin < transform.rotation.eulerAngles.z) {
-        //    //    transform.Rotate(Vector3.back, rotationSpeed);
-        //    //} else {
-        //    //    hitMax = true;
-        //    //}
-        //}
     }
 }
