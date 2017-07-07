752ea5d861c1f2062e349ab2349566bdc60b43f6
Mila <milabits@gmail.com>
2017-07-07 18:11:43 +0200

    Added new tiles and added a script to quickly add colliders to a level

---
+++ SmugglerUnity/Assets/Scripts/AddLevelColliders.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddLevelColliders : MonoBehaviour {
+
+    //This script adds a collider to every child object, primarily made to quickly add colliders to an entire level.
+    void Start() {
+        for (int i = 0; i < transform.childCount; i++) {
+            var child = transform.GetChild(i).gameObject;
+            if (child.GetComponent<Collider>() == null)
+                child.AddComponent<MeshCollider>();
+        }
+    }
+}
