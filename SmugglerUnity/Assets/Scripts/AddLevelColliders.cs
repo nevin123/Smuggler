3d8a82bfad08b276b363fd52d397a3ce38cc21fe
Mila <milabits@gmail.com>
2017-07-08 12:32:51 +0200

    Fixed light leaks and added environment layer to the level collision script

--- SmugglerUnity/Assets/Scripts/AddLevelColliders.cs
+++ SmugglerUnity/Assets/Scripts/AddLevelColliders.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AddLevelColliders : MonoBehaviour {
-
+public class AddLevelColliders : MonoBehaviour
+{
     //This script adds a collider to every child object, primarily made to quickly add colliders to an entire level.
     void Start() {
         for (int i = 0; i < transform.childCount; i++) {
             var child = transform.GetChild(i).gameObject;
             if (child.GetComponent<Collider>() == null)
                 child.AddComponent<MeshCollider>();
+            child.layer = LayerMask.NameToLayer("Environment");
         }
     }
 }
